b7eb513fd8a357052b3f33f19cfeef73b2c3183a
DemoN427395 <61602043+DemoN427395@users.noreply.github.com>
2024-02-09 01:21:02 +0300

    Update MainWindow.xaml.cs
    cleaned up comments

--- PasswordManagerV2/MainWindow.xaml.cs
+++ PasswordManagerV2/MainWindow.xaml.cs
@@ -17,9 +17,6 @@ using System.Windows.Shapes;
 
 namespace PasswordManagerV2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         AppContext db;
